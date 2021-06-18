90b0587188eb97a6d4459449a1544e5f97b510ee
dalia.elzohairy.de@gmail.com <dalia.elzohairy.de@gmail.com>
2021-06-18 21:14:31 +0200

    gameNewsFE

--- Sportify/Frontend/src/app/modules/home/gamenews/gamenews.component.ts
+++ Sportify/Frontend/src/app/modules/home/gamenews/gamenews.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NewsService } from 'src/app/services/news.service';
+import { Game, GamesControllerService, NewsDto } from 'src/app/openapi';
 @Component({
   selector: 'app-gamenews',
   templateUrl: './gamenews.component.html',
   styleUrls: ['./gamenews.component.css']
 })
 export class GamenewsComponent implements OnInit {
+  allNews: NewsDto[];
 
-  constructor(private _httpClient:HttpClient,private _newsService:NewsService) { }
+  constructor(private gamesService: GamesControllerService) { }
 
   ngOnInit(): void {
+    this.gamesService.getGameNewsUsingGET(Game.CodeNameEnum.Lol).subscribe(gamenews =>{
+      console.log(gamenews.length);
+    })
   }
 
 }
