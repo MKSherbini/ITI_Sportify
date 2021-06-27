281864f6182c9ab990ad110327bed67507d2aba1
MKSherbini <mksherbini@gmail.com>
2021-06-28 00:37:30 +0200

    false killed + news

--- Sportify/Frontend/src/app/modules/user/user.module.ts
+++ Sportify/Frontend/src/app/modules/user/user.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { MatchesHistoryComponent } from './matches-history/matches-history.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: "history", component: MatchesHistoryComponent },
-  { path: "", component: LoginComponent },
   { path: "register", component: RegistrationComponent },
 ]
 
@@ -22,10 +21,7 @@ const routes: Routes = [
     MatchesHistoryComponent,
   ],
   imports: [
-    CommonModule, ReactiveFormsModule, RouterModule, HttpClientModule, FormsModule, MatButtonToggleModule
-    , RouterModule.forChild(routes)]
-  , exports: [
-    LoginComponent
-  ]
+    CommonModule, ReactiveFormsModule, RouterModule, HttpClientModule, FormsModule, MatButtonToggleModule, RouterModule.forChild(routes)
+  ], exports: [LoginComponent]
 })
 export class UserModule { }
